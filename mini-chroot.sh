e780d6d7cb5ab79f9bf7d64c6a1668063abd4096
lunik <guillaume.lunik@gmail.com>
2018-02-15 09:53:39 +0100

    use functions

--- mini-chroot.sh
+++ mini-chroot.sh
@@ -5,52 +5,108 @@ ARCH=x86_64
 CHROOT=alpine
 VERSION=v3.7
 APK_TOOL=apk-tools-static-2.8.2-r0.apk
+DEVICE=$1
 
-# Root has $UID 0
-ROOT_UID=0
-if [ $UID -ne $ROOT_UID ]
-then
-    echo "You are not root. Please use su to become root."
-fi
+set -o xtrace
 
-if [ -d $CHROOT ]
-then
-    echo "$CHROOT already exists."
-else
-    mkdir -p $CHROOT
-fi
+function print_usage {
+    echo "USAGE:
+    $0 <INSTALL_DEVICE>
+        INSTALL_DEVICE Install alpine on this device"
+}
 
-wget $MIRROR/$VERSION/main/$ARCH/$APK_TOOL
-tar -xzf $APK_TOOL
-./sbin/apk.static \
-    -X $MIRROR/$VERSION/main \
-    -U \
-    --allow-untrusted \
-    --root ././$CHROOT \
-    --initdb add alpine-base alpine-sdk
+function init {
+    if [ -z "$DEVICE" ]
+    then
+        print_usage
+        exit 1
+    fi
 
-mkdir -p $CHROOT{/root,/etc/apk,/proc}
+    if [ "$SHELL" != "/bin/bash" ]
+    then
+        echo "Use bash to launch script"
+        #exit 1
+    fi
+}
 
-mount --bind /proc $CHROOT/proc
+function debug {
+  echo "==> $1"
+}
 
-mknod -m 666 $CHROOT/dev/full c 1 7
-mknod -m 666 $CHROOT/dev/ptmx c 5 2
-mknod -m 644 $CHROOT/dev/random c 1 8
-mknod -m 644 $CHROOT/dev/urandom c 1 9
-mknod -m 666 $CHROOT/dev/zero c 1 5
-mknod -m 666 $CHROOT/dev/tty c 5 0
-rm -f $CHROOT/dev/null
-mknod -m 666 $CHROOT/dev/null c 1 3
+function check_uid {
+  debug "Check UID"
+  ROOT_UID=0
+  if [ "$UID" -ne "$ROOT_UID" ]
+  then
+      echo "You are not root. Please use su to become root."
+      exit 0
+  fi
+}
 
-cp /etc/resolv.conf $CHROOT/etc/
-echo "$MIRROR/$VERSION/main" >  $CHROOT/etc/apk/repositories
+function create_chroot_dir {
+  debug "Create chroot directory"
+  if [ -d "$CHROOT" ]
+  then
+      echo "$CHROOT already exists."
+  else
+      mkdir -p $CHROOT
+  fi
+}
 
-# Cleaning up
-rm -rf sbin
-rm -f $APK_TOOL
+function download_apk_tool {
+  debug "Download apk tools"
+  wget $MIRROR/$VERSION/main/$ARCH/$APK_TOOL
+  tar -xzf $APK_TOOL
+}
 
-echo "\
-    Your Alpine Linux installation in '$CHROOT' is ready now.\
-    To start Alpine:\
-    sudo chroot $CHROOT /bin/sh -l\
-"
+function install_chroot {
+  debug "Install alpine in chroot"
+  ./sbin/apk.static \
+      -X $MIRROR/$VERSION/main \
+      -U \
+      --allow-untrusted \
+      --root ./$CHROOT \
+      --initdb add alpine-base alpine-sdk
+}
+
+function create_fs_arbo {
+    debug "Create fs tree"
+    mkdir -p $CHROOT{/root,/etc/apk,/proc}
+}
+
+function mount_bind {
+  debug "Mount bind"
+  mount --bind /proc $CHROOT/proc
+  mount --bind /dev $CHROOT/dev
+}
+
+function setup_repository {
+    debug "Setup alpine repository"
+    echo "$MIRROR/$VERSION/main" >  $CHROOT/etc/apk/repositories
+}
+
+function setup_resolv_conf {
+    debug "Setup resolve.conf"
+    cp /etc/resolv.conf $CHROOT/etc/
+}
+
+function cleanup_tools {
+  debug "Cleanup tools"
+  rm -rf sbin
+  rm -f $APK_TOOL
+}
+
+function Main {
+    init
+    check_uid
+    create_chroot_dir
+    download_apk_tool
+    install_chroot
+    create_fs_arbo
+    mount_bind
+    setup_repository
+    setup_resolv_conf
+    cleanup_tools
+}
+
+Main
